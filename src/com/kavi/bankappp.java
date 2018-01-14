b646965528e3b18b8f3f8e31aff41f2d6b37c8e0
kavipriya1096 <kavipriya1096dhanashekar@gmail.com>
2018-01-14 19:16:47 +0530

    commit

--- src/com/kavi/bankappp.java
+++ src/com/kavi/bankappp.java
@@ -2,46 +2,50 @@ package com.kavi;
 
 import java.util.Scanner;
 
-public class bankappp {
+ class bankappp {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+         bankof();
+         double amount=bankof();
+     }
 
-        int choice = 0;
-        double deposit, withdraw, exit;
-        double a, b;
-        double amount = 0;
-        Scanner sc = new Scanner(System.in);
+     public static double bankof() {
+         int choice = 0;
+         double deposit, withdraw, exit;
+         double a, b;
+         double amount = 0;
+         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; choice != 3; i++) {
-            System.out.println("main menu");
-            System.out.println("1.deposit");
-            System.out.println("2.withdraw");
-            System.out.println("3.exit");
-            System.out.println("enter your choice");
-            choice = sc.nextInt();
-            if (choice==1) {
+         for (int i = 0; choice != 3; i++) {
+             System.out.println("main menu");
+             System.out.println("1.deposit");
+             System.out.println("2.withdraw");
+             System.out.println("3.exit");
+             System.out.println("enter your choice");
+             choice = sc.nextInt();
+             if (choice == 1) {
 
-                System.out.println("enter your deposit ");
-                a = sc.nextDouble();
-                amount += a;
-                System.out.println("the current amount is=" + amount);
-            }
-            else if(choice==2) {
-                System.out.println("the current amount is=" + amount);
-                System.out.println("enter the withdrawl amount");
-                b = sc.nextDouble();
-                amount = amount - b;
-                System.out.println("balance is= " + amount);
-            }
-            else{
-                System.out.println("exit");
-            }
+                 System.out.println("enter your deposit ");
+                 a = sc.nextDouble();
+                 amount += a;
+                 System.out.println("the current amount is=" + amount);
+             } else if (choice == 2) {
+                 System.out.println("the current amount is=" + amount);
+                 System.out.println("enter the withdrawl amount");
+                 b = sc.nextDouble();
+                 amount = amount - b;
+                 System.out.println("balance is= " + amount);
+             } else {
+                 System.out.println("exit");
+                 return 0;
+             }
 
-        }
-    }
-}
+         }
+         return amount;
+     }
 
 
+ }
 
 
 
